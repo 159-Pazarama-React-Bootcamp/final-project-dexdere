2aa79a73cecb24d94a10bcdab9982dfe3a3ff389
Batuhan <batuhanmunger@hotmail.com>
2022-01-18 14:10:13 +0300

    Create Babel

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  //   parser: 'babel-eslint',
+  //   parserOptions: {
+  //     sourceType: 'module',
+  //     allowImportExportEverywhere: true,
+  //   },
+};
