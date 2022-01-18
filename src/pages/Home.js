952200090801d7f7a95c39795db0fb08136be49d
Batuhan <batuhanmunger@hotmail.com>
2022-01-18 19:38:26 +0300

    Add Link for home buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import css from './Home.module.css';
 
 export default function Home() {
   return (
     <div className={css.homeBody}>
       <div>
-        
-          <button className={css.createBtn} type="button">Create Ticket</button>
-          <button className={css.queryBtn} type="button">Ticket Query</button>
-        
-        <button className={css.dasboardBtn} type="button">
-          Admin Dashboard
-        </button>
+        <Link to="/create-ticket">
+          <button className={css.createBtn} type="button">
+            Create Ticket
+          </button>
+        </Link>
+
+        <Link to="/ticket-query">
+          <button className={css.queryBtn} type="button">
+            Ticket Query
+          </button>
+        </Link>
+
+        <Link to="/login">
+          <button className={css.dasboardBtn} type="button">
+            Admin Dashboard
+          </button>
+        </Link>
       </div>
     </div>
   );
